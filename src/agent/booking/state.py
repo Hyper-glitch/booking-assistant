cdda37efaf2a0d34ff2972c2f7032c826e46582b
Roman Smirnov <graff049@gmail.com>
2026-02-02 21:50:42 +0300

    feat: move contracts to dto

--- src/agent/booking/state.py
+++ src/agent/booking/state.py
@@ -1,25 +1,12 @@
-from datetime import datetime
-from typing import Annotated, Any
+from typing import Annotated
 
 from langchain_core.messages import AnyMessage
 from langgraph.graph import add_messages
-from pydantic import BaseModel
 
 from agent.booking.enums import BookingEvent
 from agent.common.state import BaseState
 from api.enums import BookingDecision
 from api.schemas import Meta
-
-
-class BookingWindow(BaseModel):
-    window_start_date_time: datetime | None = None
-    window_end_date_time: datetime | None = None
-
-
-class Booking(BaseModel):
-    address: dict[str, Any]
-    total_sla: int
-    delivery_window: BookingWindow
 
 
 class BookingState(BaseState):
