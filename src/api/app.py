f2d09b7d02f9143541beb66e3fcfb71b419f5564
Roman Smirnov <graff049@gmail.com>
2026-02-02 22:36:53 +0300

    feat: add lifespan to app

--- src/api/app.py
+++ src/api/app.py
@@ -1,12 +1,34 @@
 from collections.abc import Callable
-from typing import Any, AsyncContextManager
+from contextlib import asynccontextmanager
+from typing import Any, AsyncContextManager, AsyncGenerator
 
 from fastapi import FastAPI
+from langchain_qwq import ChatQwen
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
 
+from agent.booking.builder import create_booking_svc
 from api.metrics import APIRequestMetrics, setup_metrics
-from settings import settings
 from api.v1.routers.booking import router as agent_router
 from api.v1.routers.monitoring import router as monitoring_router
+from integration.client import ExternalAPIClient
+from settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    chat_model = ChatQwen(**settings.llm_params, streaming=False, enable_thinking=False)
+    api = ExternalAPIClient(
+        base_url=settings.EXTERNAL_API_URL,
+        token=settings.EXTERNAL_API_TOKEN,
+    )
+    app.state.booking_agent_service = create_booking_svc(
+        chat_model=chat_model,
+        api=api,
+        saver_factory=AsyncPostgresSaver.from_conn_string,
+        dsn_provider=settings.postgres_dsn,
+    )
+    yield
+    await api.close()
 
 
 def create_app(lifespan: Callable[[FastAPI], AsyncContextManager[Any]]) -> FastAPI:
@@ -28,4 +50,4 @@ def create_app(lifespan: Callable[[FastAPI], AsyncContextManager[Any]]) -> FastA
     return app
 
 
-app = create_app(settings)
+app = create_app(lifespan)
