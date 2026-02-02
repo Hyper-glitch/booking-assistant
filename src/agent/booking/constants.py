6f4b7c43b2f69f2795015ab46a146074c4115cea
Roman Smirnov <graff049@gmail.com>
2026-02-02 14:32:07 +0300

    feat: add constants

---
+++ src/agent/booking/constants.py
@@ -0,0 +1,4 @@
+MAX_CLARIFY_ANSWER_ATTEMPTS = 2
+MAX_OPERATOR_REQUESTS = 2
+TRANSFER_TO_OPERATOR_MSG = "Connecting with the operator. PLease stand by."
+CLARIFY_ANSWER = "Please rephrase, I didn't understand you."
