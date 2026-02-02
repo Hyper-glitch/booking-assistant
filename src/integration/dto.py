a9c0db974c6d4f46a2f9ab581e962042829cce87
Roman Smirnov <graff049@gmail.com>
2026-02-02 21:50:12 +0300

    feat: add contracts for external client

--- src/integration/dto.py
+++ src/integration/dto.py
@@ -1,1 +1,24 @@
-"""Contracts will be here..."""
+from datetime import datetime
+
+from openai import BaseModel
+
+
+class BookingWindow(BaseModel):
+    start_date_time: datetime
+    end_date_time: datetime
+
+
+class Booking(BaseModel):
+    booking_id: str
+    address: str
+    hotel_name: str
+    room_number: str
+    window: BookingWindow
+    total_sla: int
+    status: str
+
+
+class BookingChangeResponse(BaseModel):
+    success: bool
+    reason: str | None = None
+    updated_booking: Booking | None = None
